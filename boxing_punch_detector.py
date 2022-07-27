6ea5dbfbfa36a5adca8204b37ab3ef43f4e4778a
garrettvolpe <garrettvolpe@gmail.com>
2022-07-27 19:00:40 -0400

    add render and pose connection

--- boxing_punch_detector.py
+++ boxing_punch_detector.py
@@ -9,19 +9,33 @@ wCam, hCam = 1280, 720
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while(True):      
+        # Capture the video frame by frame
+        ret, frame = cap.read()
 
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
+        # Make detection
+        results = pose.process(image)
 
-while(True):      
-    # Capture the video frame by frame
-    ret, frame = cap.read()
-  
-    # Display the resulting frame
-    cv2.imshow('feed', frame)
-      
-    # the 'q' button is used to quit
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+        print(mp_pose.POSE_CONNECTIONS)
+        cv2.imshow('Mediapipe Feed', image)
+        
+        # the 'q' button is used to quit
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
   
 # After the loop release the cap object
 cap.release()
