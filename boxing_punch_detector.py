c03b3449abae572c56c6509e9edac2eeac778b8a
garrettvolpe <garrettvolpe@gmail.com>
2022-07-27 19:29:51 -0400

    add function for angle calculation

--- boxing_punch_detector.py
+++ boxing_punch_detector.py
@@ -30,12 +30,41 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                                   mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
                                   mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
                                   )
-        print(mp_pose.POSE_CONNECTIONS)
-        cv2.imshow('Mediapipe Feed', image)
         
+            # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+        except:
+            pass
+
+
+        def calculate_angle(a, b, c):
+            a = np.array(a)  # First
+            b = np.array(b)  # Mid
+            c = np.array(c)  # End
+
+            radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+            angle = np.abs(radians * 180.0 / np.pi)
+
+            if angle > 180.0:
+                angle = 360 - angle
+
+            return angle
+
+
+        l_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        l_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        l_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+
+        print(calculate_angle(l_shoulder, l_elbow, l_wrist))
+
+        cv2.imshow('Mediapipe Feed', image)
+                
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-  
+    
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
