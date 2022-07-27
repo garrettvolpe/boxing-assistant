d7dbc40a0457aa2ab2e3a8eec5b61ff51590384c
garrettvolpe <garrettvolpe@gmail.com>
2022-07-27 18:45:37 -0400

    add mediapipe

--- boxing_punch_detector.py
+++ boxing_punch_detector.py
@@ -1,4 +1,8 @@
 import cv2
+import mediapipe as mp 
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
 # VIDEO FEED
 wCam, hCam = 1280, 720
@@ -6,17 +10,17 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-while(True):
-      
-    # Capture the video frame
-    # by frame
+
+
+while(True):      
+    # Capture the video frame by frame
     ret, frame = cap.read()
   
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('feed', frame)
       
     # the 'q' button is used to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
   
 # After the loop release the cap object
